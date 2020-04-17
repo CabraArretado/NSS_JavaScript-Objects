3ab9593e55a1d9af409923441e19377027ba91ac
Felipe M <42254622+felipetempus@users.noreply.github.com>
2020-04-17 14:38:10 -0400

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-let commitMessage;
-for (let i = 0; i < githubData.length; i++) {
-    if (githubData[i]["id"] === "8030403992") {
-        commitMessage = githubData[i]["payload"]["commits"][0]["message"]
-    }
-}
